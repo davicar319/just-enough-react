039fbad0d44cffb0a484ed1d52409cddd0100f74
Dave Carlson <dave.carlson@tr.com>
2020-03-26 15:29:32 -0500

    Adding sparkle functionality to the hello world page.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Sparkle from "./Sparkle";
 
 function App() {
-  return (
-    <div className="App">
-      <p>Hello world!</p>
-    </div>
-  );
+    const name = 'Dave';
+    const now = String(new Date());
+    return (
+        <div className="App">
+            <p>Hello {name}!</p>
+            <p>The current time is {now}.</p>
+            <p>2 + 2 = {2 + 2} </p>
+            <Sparkle/>
+        </div>
+    );
 }
 
 export default App;
